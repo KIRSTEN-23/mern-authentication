2cc308b322b33835992a578c11af2f81076d6930
Kirsten231296 <125568773+KIRSTEN-23@users.noreply.github.com>
2026-05-14 11:06:54 +0200

    Project Setup
    File setup; Install, update and confirm dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="p-5">
+      <h1>MERN Luxury Accommodation</h1>
+
+      <Button variant="dark">
+        Bootstrap Working
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI,)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
